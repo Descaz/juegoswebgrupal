de63fe42885324c4de2566d4cde0eec324865037
DanielHUnir <daniel.heras789@comunidadunir.net>
2026-05-30 18:46:50 +0200

    Revert "+"
    This reverts commit dc2db8df411ed6315d7d72075cfee00b02dfaf1e.

--- js/escenaBase.js
+++ js/escenaBase.js
@@ -1,4 +1,7 @@
 import Personaje from './personaje.js';
+import Enemigo from './enemigo.js';
+import Moneda from './moneda.js';
+import Pinchos from './pinchos.js';
 import BalaGroup from './bala.js';
 import Bala from './bala.js';
 import Gusano from './Gusano.js';
