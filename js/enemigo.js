8ccd688128d54a983fa6a387361b39e358b931cd
DanielHUnir <daniel.heras789@comunidadunir.net>
2026-05-30 18:46:45 +0200

    Revert "limpieza"
    This reverts commit 5ef3e28d83e54dda33829d3f85ebd38115742a86.

---
+++ js/enemigo.js
@@ -0,0 +1,17 @@
+export default class Enemigo extends Phaser.Physics.Arcade.Sprite {
+  constructor(escena, x, y) {
+    super(escena, x, y, 'gusano_frame1');
+    this.escena = escena;
+    this.escena.add.existing(this);
+    this.escena.physics.add.existing(this);
+    this.setCollideWorldBounds(true);
+    this.setBounce(0.1); // Rebote completo (100%)
+    const velocidadX = Phaser.Math.Between(-150, 150);
+    const velocidadY = Phaser.Math.Between(-150, 150);
+    this.setVelocity(velocidadX, velocidadY);
+  }
+   
+  update() {
+    
+  }
+}
