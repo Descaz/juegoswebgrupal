d5cb7ac9d76715e0c3614bbd1dce6b2756cf3e90
DanielHUnir <daniel.heras789@comunidadunir.net>
2026-06-01 22:12:36 +0200

    fuera debug

--- js/juego.js
+++ js/juego.js
@@ -23,7 +23,7 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 500},
-            debug: true
+            debug: false
         }
     },
     scene: [Start, EscenaBase]
